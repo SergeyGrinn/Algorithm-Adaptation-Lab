761a8bec8deb0816f15860bc17f4c37b7048fe5a
SergeyGrinn <sgrinkevich82@gmail.com>
2025-12-01 15:43:45 +0200

    Fifth task finished

---
+++ task4.py
@@ -0,0 +1,14 @@
+arr = ["Melon", "Apple", "Zebra", "Grape"]
+def insertion_sort_desc_strings(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j].lower() < key.lower():
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+print(insertion_sort_desc_strings(arr))
+
