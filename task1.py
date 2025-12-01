77e97b3cad8e009ef2f579d8d6c44bcb9a31851b
SergeyGrinn <sgrinkevich82@gmail.com>
2025-12-01 14:44:52 +0200

    First task finished

---
+++ task1.py
@@ -0,0 +1,11 @@
+
+arr = [20, 5, 1, 8, 15, 3]
+def bubble_sort_desc(arr):
+    n = len(arr)
+    for j in range(n):
+        for i in range(n-1):
+            if arr[i] < arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+    return arr
+
+print(bubble_sort_desc(arr))
