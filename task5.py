9180a2b614fa8fd0341945f48de52ffb5bb9d872
SergeyGrinn <sgrinkevich82@gmail.com>
2025-12-01 15:53:21 +0200

    Fifth task finished

---
+++ task5.py
@@ -0,0 +1,12 @@
+arr = [7, 3, 5, 3, 1, 7]
+def selection_sort_with_duplicates(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+print(selection_sort_with_duplicates(arr))
